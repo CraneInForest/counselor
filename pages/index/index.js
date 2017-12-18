bcfb56a542de4b081d7748b906e0f84a52717f6b
林鹤 <linhe@linhes-mbp.local>
2017-12-18 16:27:52 +0800

    替换了第一层界面的UI图标和文字

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,9 +12,9 @@ Page({
     iconsrc:'../../image/usercount.png',
     jtsrc:'../../image/icon-jt.png',
     imgUrls: [
-      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1495626232647&di=c7c53f96e0f48681471e4626eebe0181&imgtype=0&src=http%3A%2F%2Fwww.sanchiseo.com%2Fuploadfile%2F2015821%2F2015821115728937045.jpg',
-      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1495626316444&di=ff20f74da6031541a12e0eeadaf156b9&imgtype=0&src=http%3A%2F%2Fsem.g3img.com%2Fsite%2F34016275%2F20160216162430_82108.png',
-      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1495626359075&di=3297d75c3742024d15f553547495f3db&imgtype=0&src=http%3A%2F%2Fwww.17emarketing.com%2Fuploads%2Fallimg%2F160627%2F1-16062G54154.png',
+      '../../image/m1.png',
+      '../../image/m2.png',
+      '../../image/m3.png',
     ],
     indicatorDots: true,
     autoplay: true,
