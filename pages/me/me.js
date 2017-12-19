c753794d73aea50d582dc03588383f9a3189113c
林鹤 <linhe@linhes-mbp.local>
2017-12-19 11:28:27 +0800

    完善了"我的"界面的正式内容

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,16 +1,47 @@
-//index.js
-//获取应用实例
 const app = getApp()
 
 Page({
   data: {
+    arraykemu: ['理科', '文科'],
+    arrayshengfen: ['安徽', '澳门', '北京', '重庆', '福建', '甘肃', '广东', '广西', '贵州', '海南', '河北', '河南', '黑龙江', '湖北', '湖南','吉林','江苏','江西','辽宁','内蒙古','宁夏','青海','山东','山西','陕西','上海','四川','台湾','天津','西藏', '香港', '新疆', '云南', '浙江'],
+    arraypici: ['本科一批', '本科二批', '本科三批', '专科'],
+    index: 0,
+    index1: 0,
+    index2: 0,
+    date: '2016-09-01',
+    time: '12:01',
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+  bindPickerChange: function(e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindPickerChange1: function (e) {
+    this.setData({
+      index1: e.detail.value
+    })
+  },
+  bindPickerChange2: function (e) {
+    this.setData({
+      index2: e.detail.value
+    })
+  },
+  bindDateChange: function(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function(e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -26,8 +57,7 @@ Page({
       }
     })
   },
-  getUserInfo: function(e) {
-    console.log("e=====")
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
